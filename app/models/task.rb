f49a5031f9554ebf302147b4825f038e8f6cdd4a
tomoya.ak <onetime1122tist@outlook.jp>
2022-07-05 12:36:13 +0000

    kadai-2-2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     
-  validates :conntent, presence: true, length: { maximum: 10 }
+  validates :conntent, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
     
 end
