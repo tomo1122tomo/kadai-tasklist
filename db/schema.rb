c69f5a9d40516c3f5e5182702388ce0da8d913af
tomoya.ak <onetime1122tist@outlook.jp>
2022-07-15 05:34:27 +0000

    kadai-tasklist-set

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_003636) do
+ActiveRecord::Schema.define(version: 2022_07_15_051106) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "conntent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
